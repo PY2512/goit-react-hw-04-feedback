d2c5a367f36d4399729705dbf2ac0fc7ab93d170
PY2512 <58562352+PY2512@users.noreply.github.com>
2022-04-22 21:17:03 +0300

    react-hooks added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,44 @@
-import {Component} from "react";
+import {useState} from "react";
 import Buttons from "./Buttons/Buttons";
 import SectionTitle from "./Title/SectionTitle"
 import Statistics from "./Statistics/Statistics";
 import Notification from "./Notification/Notification";
 
 
+const App = () => {
+  const [state, setState] = useState({good: 0, neutral: 0, bad:0});
+  const {neutral, good, bad} = state;
 
-class App extends Component {
-  
-  state = { good: 0, neutral:0, bad:0 };
+  const handleIncrement = event => {
+    const {id} = event.target;
+    setState(prevstate => ({...prevstate, [id]:prevstate[id] + 1}))
+    // this.setState((prevstate) => ({[id]:prevstate[id] + 1}));
+};
 
-  handleIncrement = (event) => { 
-    const {id} = event.target; 
-    this.setState((prevstate) => ({[id]:prevstate[id] + 1})
-    )};
+const countTotalFeedback = () => {
+        return good + neutral + bad;
+};
 
-    countTotalFeedback = () => {
-      const {good, neutral, bad} = this.state;
-      return good + neutral + bad;
-    };
-    
-    countPositiveFeedbackPercentage = () => {
-      const {good, neutral, bad} = this.state;
-      const sum = good + neutral + bad;
-      const result = (good * 100) / sum;
-      return result ? Math.floor(result) : "0"
-    };
+const countPositiveFeedbackPercentage = () => {
+        const sum = good + neutral + bad;
+        const result = (good * 100) / sum;
+        return result ? Math.floor(result) : "0"
+};
 
-  render() {
-    const {good, neutral, bad} = this.state;
-    const {countTotalFeedback,  handleIncrement} = this;
-    return (
-      <>
-        <SectionTitle />
-        <Buttons handleIncrement={handleIncrement}/>
-        {countTotalFeedback() === 0 ? (<Notification message="No feedback given" />) : 
-        <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-        />}
-      </>
-      
-    );
-  }
+return (
+  <>
+    <SectionTitle />
+    <Buttons handleIncrement={handleIncrement}/>
+    {countTotalFeedback() === 0 ? (<Notification message="No feedback given" />) : 
+      <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+      />}
+  </>
+)
 }
-
-
 
 export default App;
